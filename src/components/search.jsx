ddd2e4bb79b4ed49add083d0fad35bb3b002da44
Ayush Meena <ayushmeena22@iitk.ac.in>
2024-03-18 22:13:24 +0530

    first commit

---
+++ src/components/search.jsx
@@ -0,0 +1,48 @@
+// Search.jsx
+import React, { useState } from 'react';
+import SearchForm from './SearchForm';
+import SearchResult from './SearchResult';
+import SearchImage from './images/11000.png'
+import "./search.css";
+
+const Search = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (data) => {
+    try {
+      // Check if either college or major is empty
+      if (!data.college || !data.major) {
+        alert("Please select all fields.");
+        return; // Stop execution if fields are empty
+      }
+      
+      // Make API request to search for students
+      // You need to replace the API endpoint with your actual endpoint
+      const response = await fetch(`http://localhost:5000/users/search?college=${data.college}&branch=${data.major}`);
+      const searchData = await response.json();
+      setSearchResults(searchData);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
+  
+
+  return (
+    <div>
+      <div className="up">
+          <div className="home-main-text">
+              <h1>College Search</h1>
+          </div>
+          <div className="image-container">
+              <img src={SearchImage} className="home_img" alt="" />
+          </div>
+      </div>
+      <div>
+      </div>
+      <SearchForm onSearch={handleSearch} />
+      <SearchResult students={searchResults} />
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const api = {  
+  tagsEndPoint: 'https://college-backend-tyqu.onrender.com/tags/',
+  usersEndPoint: 'https://college-backend-tyqu.onrender.com/users/',
+  postsEndPoint: 'https://college-backend-tyqu.onrender.com/posts/',
+  repliesEndPoint: 'https://college-backend-tyqu.onrender.com/reply/',
+}
