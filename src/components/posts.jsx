10171f2bea40713063cbc2c3e03d640b2a6f6f82
Ayush Meena <121339676+AYushM25@users.noreply.github.com>
2024-03-30 10:38:30 +0530

    Update posts.jsx

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -8,21 +8,18 @@ const Posts = (props) => {
       {posts.map((post) => (
         <Link
           className="list-group-item list-group-item-action flex-column align-items-start"
-          to={'/post/${post._id}'}
-          key={post._id}
+          to={`/post/${post._id}`}
         >
-          <div className="d-flex w-100 justify-content-between">
+          <div className="d-flex w-100 justify-content-between" key={post._id}>
             <h5 className="mb-1">{post.title}</h5>
           </div>
-          {post.author && (  // Check if post.author exists before accessing its properties
-            <small>Created by {post.author.name}</small>
-          )}
+          <small>Created by {post.author.name}</small>
           <br />
           <small className="overflow-hidden">{post.description}</small>
           <div className="mt-1">
             Related Topics:
             {post.tags.map((tag) => (
-              <span className="badge badge-secondary m-1 p-2" key={tag._id}>{tag.name}</span>
+              <span className="badge badge-secondary m-1 p-2">{tag.name}</span>
             ))}
             <h6 className="mt-2">
               {post.upvotes.length} Likes | {post.views} Views
