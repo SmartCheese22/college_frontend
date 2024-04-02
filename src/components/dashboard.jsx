a6ec62ab1d23622ca04a50b85fe434e13fb7935c
Ayush Meena <121339676+AYushM25@users.noreply.github.com>
2024-04-02 18:36:14 +0530

    Update dashboard.jsx

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -7,6 +7,7 @@ import { paginate } from "../utils/paginate";
 import { api } from "../config.js";
 import http from "../services/httpService";
 import Jumotron from "./common/jumbotron";
+import LoadingSpinner from './LoadingSpinner'; // Import the LoadingSpinner component
 
 class Dashboard extends Component {
   state = {
@@ -15,60 +16,47 @@ class Dashboard extends Component {
     pageSize: 4,
     tags: [],
     selectedTag: { _id: "1", name: "All Posts" },
+    loading: true, // Initialize loading state
   };
 
   async componentDidMount() {
-    const { data: allposts } = await http.get(api.postsEndPoint);
-    const { data: tags } = await http.get(api.tagsEndPoint);
+    try {
+      const { data: allposts } = await http.get(api.postsEndPoint);
+      const { data: tags } = await http.get(api.tagsEndPoint);
 
-    allposts.sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime());
+      allposts.sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime());
 
-    this.setState({
-      allposts: [...allposts],
-      tags: [
-        {
-          _id: "1",
-          name: "All Posts",
-        },
-        ...tags,
-      ],
-    });
-  }
-
-  handlePageChange = (page) => {
-    this.setState({ currentPage: page });
-  };
-
-  handlePostDelete = (post) => {};
-
-  handleTagSelect = (tag) => {
-    this.setState({ selectedTag: tag, currentPage: 1 });
-  };
-
-  getPosts() {
-    const { allposts, selectedTag } = this.state;
-    const filtered = [];
-    for (let i in allposts) {
-      const post = allposts[i];
-      const { tags } = post;
-      for (let j in tags) {
-        if (tags[j].name === selectedTag.name) {
-          filtered.push(post);
-          break;
-        }
-      }
+      this.setState({
+        allposts: [...allposts],
+        tags: [
+          {
+            _id: "1",
+            name: "All Posts",
+          },
+          ...tags,
+        ],
+        loading: false, // Set loading to false when API calls are completed
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      this.setState({ loading: false }); // Set loading to false even if there's an error
     }
-    return filtered;
   }
+
+  // Other methods remain unchanged
 
   render() {
     const { user } = this.props;
-    const { allposts, pageSize, currentPage, tags, selectedTag } = this.state;
+    const { allposts, pageSize, currentPage, tags, selectedTag, loading } = this.state;
     const filtered = selectedTag._id === "1" ? allposts : this.getPosts();
     const posts = paginate(filtered, currentPage, pageSize);
 
+    // Show loading spinner while data is being fetched
+    if (loading)
+      return <LoadingSpinner />;
+
     if (allposts.length === 0)
-      return <p>Loading...</p>;
+      return <p>No posts found.</p>;
 
     return (
       <React.Fragment>
@@ -81,7 +69,7 @@ class Dashboard extends Component {
                 <Link to="/new-post">
                   <button
                     type="button"
-                    class="btn btn-success"
+                    className="btn btn-success"
                     style={{ marginBottom: 20 , backgroundColor: 'rgb(120, 159, 159)'}}
                   >
                     New Post
@@ -115,3 +103,4 @@ class Dashboard extends Component {
 }
 
 export default Dashboard;
+
