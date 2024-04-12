7a3185d241644c6fa97b6f1a5ed118b3320f385c
Anitejjain <131655465+Anitejjain@users.noreply.github.com>
2024-04-12 15:20:06 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ export const api = {
   usersEndPoint: 'https://college-backend-tyqu.onrender.com/users/',
   postsEndPoint: 'https://college-backend-tyqu.onrender.com/posts/',
   repliesEndPoint: 'https://college-backend-tyqu.onrender.com/reply/',
+  otpEndPoint: 'https://college-backend-tyqu.onrender.com/otp/',
 }
